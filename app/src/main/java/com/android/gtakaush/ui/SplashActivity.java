a5098db213f5a38a151053fd7af354f7349b7e98
969rishi <969rishi@gmail.com>
2020-01-16 02:22:08 +0530

    16-01-20 Second commit - GTA Academy libraries updated splashscreen updated

---
+++ app/src/main/java/com/android/gtakaush/ui/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.android.gtakaush.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.gtakaush.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
